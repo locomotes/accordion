aad1393907ca15508931ec95a05d9bf1721d1bcf
Jonathan Dickerson <jonathandickerson@Jonathans-MacBook-Pro-4.local>
2014-06-16 20:48:55 -0400

    three section accordion

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,24 @@
-console.log('\'Allo \'Allo!');
+$('.box1tab').click(function() {
+    $('.box1info').show();
+    $('.box2info').hide();
+    $('.box3info').hide();
+    // $(this) // $('.box1tab')
+});
+
+// // $('.boxtab').click(function() {
+
+// //     $('.boxinfo').hide();
+// //     $(this) // $('.box2tab')
+// });
+
+$('.box2tab').click(function() {
+    $('.box1info').hide();
+    $('.box2info').show();
+    $('.box3info').hide();
+});
+
+$('.box3tab').click(function() {
+    $('.box1info').hide();
+    $('.box2info').hide();
+    $('.box3info').show();
+});
